5ab5985ceb016d534e1ec8189edfc8cadb28960b
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-04-24 19:29:42 -0400

    step 6 completed

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -8,7 +8,9 @@ function CurrentUserProvider({ children }){
     const [currentUser, setCurrentUser] = useState(null)
     useEffect(() => {
         const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:5000/authentication/profile')
+            let response = await fetch('http://localhost:5000/authentication/profile', {
+                credentials: 'include'
+            })
             let user = await response.json()
             setCurrentUser(user)
         }
