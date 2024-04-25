f9d5a1fb819d3b88bb31a5772213f4acd0d9f75d
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-04-24 20:25:23 -0400

    step 5 completed

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -9,7 +9,9 @@ function CurrentUserProvider({ children }){
     useEffect(() => {
         const getLoggedInUser = async () => {
             let response = await fetch('http://localhost:5000/authentication/profile', {
-
+                header: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
             })
             let user = await response.json()
             setCurrentUser(user)
