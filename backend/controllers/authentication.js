89edfdee85805c827de7b54f1fc6afff641f8de3
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-04-24 20:33:17 -0400

    step 6 completed

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -24,18 +24,32 @@ router.post('/', async (req, res) => {
 
 
 // retrieves user profile
-router.get('/profile', async (req,res) => {
-    console.log(req.session.userId)
+router.get('/profile', async (req, res) => {
     try {
-        // let user = await User.findOne({
-        //     where: {
-        //         userId: 
-        //     }
-        // })
-        // res.json(user)
+        // split the authorization header into [ "Bearer", "TOKEN"]
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+        // only handle Bearer for now
+        if (authenticationMethod == 'Bearer') {
+            // decode JWT
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+            // get logged in user's id
+            const { id } = result.value
+
+            // 
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+
+            res.json(user)
+        }
     } catch {
         res.json(null)
     }
+    
 })
 
 module.exports = router
